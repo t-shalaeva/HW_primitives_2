2e1e0fcd221a4547652cb7d87c4a5e5e85bbb79e
Shalaeva T <alice.contist@gmail.com>
2024-07-30 19:58:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int balance = 50;
+        int depositAmount=2389;
+        int totalAmount;
+
+        if (depositAmount > 1000) {
+            totalAmount = depositAmount/100;
+            balance = balance + depositAmount + totalAmount;
+        }
+        else {
+            totalAmount = 0;
+            balance = balance + depositAmount;
+        }
+        System.out.println("Итоговый счёт: " + balance + " рублей, количество бонусных рублей: " + totalAmount);
+
+    }
+}
